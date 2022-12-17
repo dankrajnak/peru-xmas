8fbf27581a76c6bf0bed01bbcbde548f9ff27d6f
Dan <dan.krajnak@gmail.com>
2022-12-17 17:43:04 +0200

    Fix ts errors

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -2,25 +2,15 @@ import Avatar from "./avatar";
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import Link from "next/link";
-import type Author from "../interfaces/author";
 
 type Props = {
   title: string;
   coverImage: string;
-  date: string;
   excerpt: string;
-  author: Author;
   slug: string;
 };
 
-const PostPreview = ({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: Props) => {
+const PostPreview = ({ title, coverImage, excerpt, slug }: Props) => {
   return (
     <div>
       <div className="mb-5">
