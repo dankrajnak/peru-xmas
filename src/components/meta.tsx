60e938e01cbe123895c5b7a74d8b5a5204c510ee
Dan <dan.krajnak@gmail.com>
2022-12-24 17:44:24 +0200

    meta stuff

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import Head from "next/head";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 
 const Meta = () => {
   return (
@@ -31,14 +31,9 @@ const Meta = () => {
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta name="description" content={`Things to do with Dan in Peru.`} />
     </Head>
-  )
-}
+  );
+};
 
-export default Meta
+export default Meta;
