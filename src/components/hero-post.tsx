e52930cbebb58fab5c9f5c66829e71537918a526
Dan <dan.krajnak@gmail.com>
2022-12-24 17:42:12 +0200

    Slight text size adjustment

--- src/components/hero-post.tsx
+++ src/components/hero-post.tsx
@@ -16,7 +16,7 @@ const HeroPost = ({ title, coverImage, excerpt, slug }: Props) => {
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
-          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">{title}</h3>
+          <h3 className="mb-4 text-2xl lg:text-5xl leading-tight">{title}</h3>
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
