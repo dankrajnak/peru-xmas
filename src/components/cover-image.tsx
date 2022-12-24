4457c9efa588f89b9163fae6ac43906b0bcecbd9
Dan <dan.krajnak@gmail.com>
2022-12-24 17:00:17 +0200

    Image priority fix

--- src/components/cover-image.tsx
+++ src/components/cover-image.tsx
@@ -6,9 +6,10 @@ type Props = {
   title: string;
   src: string;
   slug?: string;
+  priority?: boolean | null;
 };
 
-const CoverImage = ({ title, src, slug }: Props) => {
+const CoverImage = ({ title, src, slug, priority }: Props) => {
   const image = (
     <Image
       src={src}
@@ -18,7 +19,7 @@ const CoverImage = ({ title, src, slug }: Props) => {
       })}
       width={1300}
       height={630}
-      priority
+      priority={priority}
     />
   );
   return (
